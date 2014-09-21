4e9aa541e1978238075d8828179ac0363546c786
Nat Morris <nat@nuqe.net>
2014-09-21 14:58:39 -0700

    v6 address for 2s puppet bridges

--- pkgs/workbench/cldemo-wbench-ospfunnum-2s-puppet/debian/etc/puppet/manifests/site.pp
+++ pkgs/workbench/cldemo-wbench-ospfunnum-2s-puppet/debian/etc/puppet/manifests/site.pp
@@ -4,8 +4,8 @@ node 'leaf1.lab.local' {
     $int_loopback6 = '2001:db8:2:2000::1'
     $int_unnumbered = [ 'swp1', 'swp2', 'swp3', 'swp4' ]
     $int_bridges = {
-        br0 => { 'address' => '10.4.1.1', 'netmask' => '255.255.255.128', 'members' => ['swp30','swp31','swp32','swp33'] },
-        br1 => { 'address' => '10.4.1.129', 'netmask' => '255.255.255.128', 'members' => ['swp34','swp35','swp36','swp37'] }
+        br0 => { 'address' => '10.4.1.1', 'netmask' => '255.255.255.128', 'address6' => '2001:db8:2:4001::1/64',  'members' => ['swp30','swp31','swp32','swp33'] },
+        br1 => { 'address' => '10.4.1.129', 'netmask' => '255.255.255.128', 'address6' => '2001:db8:2:4002::1/64', 'members' => ['swp34','swp35','swp36','swp37'] }
     }
     include ospfunnum::role::switchbase
 }
@@ -16,8 +16,8 @@ node 'leaf2.lab.local' {
     $int_loopback6 = '2001:db8:2:2000::2'
     $int_unnumbered = [ 'swp1', 'swp2', 'swp3', 'swp4' ]
     $int_bridges = {
-        br0 => { 'address' => '10.4.2.1', 'netmask' => '255.255.255.128', 'members' => ['swp30','swp31','swp32','swp33'] },
-        br1 => { 'address' => '10.4.2.129', 'netmask' => '255.255.255.128', 'members' => ['swp34','swp35','swp36','swp37'] }
+        br0 => { 'address' => '10.4.2.1', 'netmask' => '255.255.255.128', 'address6' => '2001:db8:2:4003::1/64', 'members' => ['swp30','swp31','swp32','swp33'] },
+        br1 => { 'address' => '10.4.2.129', 'netmask' => '255.255.255.128', 'address6' => '2001:db8:2:4004::1/64', 'members' => ['swp34','swp35','swp36','swp37'] }
     }
     include ospfunnum::role::switchbase
 }
