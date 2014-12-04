f9b5b5e1bfc364a745084ba1a13898723152dce7
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2014-12-04 13:14:06 +0000

    Add sysctl cookbook
    Add the sysctl cookbook as a submodule
    Upload the cookbook during post-install
    Make the cldemo_base cookbook depend on the sysctl cookbook

--- pkgs/workbench/cldemo-wbench-base-chef/debian/usr/local/share/chef/cookbooks/cldemo_base/metadata.rb
+++ pkgs/workbench/cldemo-wbench-base-chef/debian/usr/local/share/chef/cookbooks/cldemo_base/metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'mwillsher@cumulusnetworks.com'
 license          'All rights reserved'
 description      'Installs/Configures cldemo_base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.2.0'
+
+depends          'sysctl'
