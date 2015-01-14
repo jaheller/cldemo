1130dcf41bf4339a73a65e423eb1b48d60714e4b
Leslie Carr <geekgirl@gmail.com>
2015-01-13 16:48:48 -0800

    adding 2lt22s version for ganglia demo

---
+++ pkgs/workbench/cldemo-wbench-ganglia-2lt22s-puppet/debian/etc/puppet/manifests/site.pp
@@ -0,0 +1,59 @@
+node 'spine1.lab.local' {
+    $int_enabled = true
+    $int_loopback = '10.2.1.3'
+    $int_unnumbered = [ 'swp49', 'swp50', 'swp51', 'swp52' ]
+    $int_bridges = { }
+    include monitoring::role::gangliaswitch
+}
+
+node 'spine2.lab.local' {
+    $int_enabled = true
+    $int_loopback = '10.2.1.4'
+    $int_unnumbered = [ 'swp49', 'swp50', 'swp51', 'swp52' ]
+    $int_bridges = { }
+    include monitoring::role::gangliaswitch
+}
+
+node 'leaf1.lab.local' {
+    $int_enabled = true
+    $int_loopback = '10.2.1.1'
+    $int_unnumbered = [ 'swp1s0', 'swp1s1', 'swp1s2', 'swp1s3' ]
+    $int_bridges = {
+        br0 => {  'address' => '10.4.1.1',
+                  'netmask' => '255.255.255.128',
+                  'members' => ['swp32s0'] },
+        br1 => {  'address' => '10.4.1.129',
+                  'netmask' => '255.255.255.128',
+                  'members' => ['swp32s1'] }
+    }
+    include monitoring::role::gangliaswitch
+    class { 'portsconf' :
+      switchtype => '40G',
+      stage      => 'setup',
+    }
+}
+
+node 'leaf2.lab.local' {
+    $int_enabled = true
+    $int_loopback = '10.2.1.2'
+    $int_unnumbered = [ 'swp1s0', 'swp1s1', 'swp1s2', 'swp1s3' ]
+    $int_bridges = {
+        br0 => {  'address' => '10.4.2.1',
+                  'netmask' => '255.255.255.128',
+                  'members' => ['swp32s0'] },
+        br1 => {  'address' => '10.4.2.129',
+                  'netmask' => '255.255.255.128',
+                  'members' => ['swp32s1'] }
+    }
+    include monitoring::role::gangliaswitch
+    class { 'portsconf' :
+      switchtype => '40G',
+      stage      => 'setup',
+    }
+node 'wbench.lab.local' {
+    $data_sources = [ 'wbench.lab.local' ]
+    $websites = [
+      { title => 'Ganglia', location => 'ganglia/' }
+    ]
+    include monitoring::role::gangliawbench
+}
